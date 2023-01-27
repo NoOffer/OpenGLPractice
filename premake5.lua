035a7c596d7079ecf34d34e59c24b6f74b12e7a6
Nofer <sxx261@case.edu>
2023-01-26 23:27:10 -0500

    Created premake5.lua (unfinished)

---
+++ premake5.lua
@@ -0,0 +1,33 @@
+workspace "OpenGLPractice"
+    architecture "x64"
+	language "C++"
+    
+	configurations
+	{
+		"Debug",
+		"Release"
+	}
+
+	filter "configurations:Debug"
+        symbols "On"
+
+    filter "configurations:Release"
+        optimize "On"
+
+outputdir = "%{cfg.system}-%{cfg.architecture}/%{cfg.buildcfg}/%{prj.name}"
+
+project "OpenGLPractice"
+    location "OpenGLPractice"
+
+	targetdir ("bin/" .. outputdir)
+	objdir ("bin-obj/" .. outputdir)
+
+	includedirs
+	{
+		"dependencies/GLFW/include"
+	}
+
+	links
+	{
+		"GLFW"
+	}
