9dbffb47497ed46ace5665e4ab9a029817036019
Nofer <sxx261@case.edu>
2023-01-30 21:02:40 -0500

    Update HelloWorld.cpp

--- OpenGLPractice/HelloWorld.cpp
+++ OpenGLPractice/HelloWorld.cpp
@@ -2,7 +2,46 @@
 #include <glad/glad.h>
 #include <GLFW/glfw3.h>
 
+int WIDTH = 800, HEIGHT = 600;
+
 int main()
 {
-	std::cout << "Hello World!" << std::endl;
+	//std::cout << "Hello World!" << std::endl;
+
+	// OpenGL context initialization
+	glfwInit();
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+	// Window initialization
+	GLFWwindow* window = glfwCreateWindow(WIDTH, HEIGHT, "LearnOpenGL", NULL, NULL);
+	if (window == NULL) {
+		std::cout << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return -1;
+	}
+	glfwMakeContextCurrent(window);
+
+	// GLAD initialization
+	if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+		std::cout << "Failed to initialize GLAD" << std::endl;
+		return -1;
+	}
+
+	// Set viewport
+	glViewport(0, 0, WIDTH, HEIGHT);
+	glfwSetFramebufferSizeCallback(window, [](GLFWwindow* window, int width, int height) {
+		glViewport(0, 0, width, height);
+		});
+
+	// Main loop
+	while(!glfwWindowShouldClose(window))
+	{
+		glfwSwapBuffers(window);
+		glfwPollEvents();
+	}
+
+	glfwTerminate();
+	return 0;
 }
