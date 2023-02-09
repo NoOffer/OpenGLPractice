b23db22ba930bb898fec4a77dead59a3b474a9f7
Nofer <sxx261@case.edu>
2023-02-08 23:57:23 -0500

    Update Main.cpp

--- OpenGLPractice/src/Main.cpp
+++ OpenGLPractice/src/Main.cpp
@@ -10,6 +10,7 @@
 
 // Custom abstractions
 #include "Window.h"
+#include "Model.h"
 #include "VertexArray.h"
 #include "IndexBuffer.h"
 #include "Shader.h"
@@ -63,31 +64,11 @@ int main(void)
 	// Print out current OpenGL version
 	std::cout << glGetString(GL_VERSION) << std::endl;
 
-	// Render content initialization
 	{
-		// Vertices
-		float vertPos[16] = {
-			// Position   UV Coord
-			-1.0f, -1.0f, 0.0f, 0.0f,  // 0
-			 1.0f, -1.0f, 1.0f, 0.0f,  // 1
-			 1.0f,  1.0f, 1.0f, 1.0f,  // 2
-			-1.0f,  1.0f, 0.0f, 1.0f,  // 3s
-		};
-		VertexBuffer vb(vertPos, sizeof(float) * 16);	  // Create vertext buffer
-		VertexBufferLayout layout;						  // Create buffer layout
-		// Setup layout
-		layout.Push<float>(2);
-		layout.Push<float>(2);
-		VertexArray va;									  // Create vertex array
-		// Bind vb and layout to va
-		va.SetupArray(vb, layout);
-
-		// Indices
-		unsigned int vertIdx[6] = {
-			0, 1, 2,
-			0, 2, 3
-		};
-		IndexBuffer ib(vertIdx, 6);						  // Create index buffer
+		// Render content initialization
+		Model model;
+		model.Translate(glm::vec3(320.0f, 240.0f, 0.0f));
+		model.Scale(glm::vec3(200.0f, 200.0f, 1.0f));
 
 		ShaderProgram shader(							  // Create shader
 			"res/shaders/TestVert.shader",
@@ -97,7 +78,7 @@ int main(void)
 		// Projection matrix
 		glm::mat4 projMatrix = glm::ortho(0.0f, 640.0f, 0.0f, 480.0f, 0.0f, 1.0f);
 		glm::mat4 viewMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0, 0, 0));
-		glm::mat4 modelMatrix = glm::scale(glm::translate(glm::mat4(1.0f), glm::vec3(320.0f, 240.0f, 0.0f)), glm::vec3(200.0f, 200.0f, 1.0f));
+		glm::mat4 modelMatrix = model.GetModelMatrix();
 		shader.SetUniformMat4f("u_MVP", projMatrix * viewMatrix * modelMatrix);
 
 		Texture texture("res/textures/TestTexture.png");  // Create texture
@@ -114,8 +95,7 @@ int main(void)
 			// Clear
 			glClear(GL_COLOR_BUFFER_BIT);
 
-			va.Bind();
-			ib.Bind();
+			model.Bind();
 			shader.Bind();
 			// Drawcall
 			glDrawElements(GL_TRIANGLES, ib.GetCount(), ib.GetIndexType(), nullptr);
