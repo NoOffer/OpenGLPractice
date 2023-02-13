611914bf85f5af8f4c2c0bc158516a599cb5beba
Nofer <sxx261@case.edu>
2023-02-12 21:18:13 -0500

    Updated view matrix

--- OpenGLPractice/src/Camera.cpp
+++ OpenGLPractice/src/Camera.cpp
@@ -6,8 +6,15 @@ Camera::~Camera() {}
 
 glm::mat4 Camera::GetViewMatrix()
 {
-	glm::vec3 forward(glm::normalize(m_Position));
-	glm::vec3 right(glm::cross(glm::vec3(1.0f, 1.0f, 1.0f), forward));
-	glm::vec3 upward(glm::cross(forward, right))
-	return glm::mat4();
+	glm::vec3 backward(glm::normalize(m_Position));  // By convention the z axis of camera points backward
+	glm::vec3 right(glm::cross(glm::vec3(0.0f, 1.0f, 0.0f), backward));
+	glm::vec3 upward(glm::cross(backward, right));
+	//return glm::translate(glm::mat4(1.0f), -m_Position);
+	return glm::lookAt(m_Position, glm::vec3(0.0f, 0.0f, 0.0f), upward);
+	//return glm::mat4(
+	//	right.x, upward.x, forward.x, m_Position.x,
+	//	right.y, upward.y, forward.y, m_Position.y,
+	//	right.z, upward.z, forward.z, m_Position.z,
+	//	0.0f, 0.0f, 0.0f, 0.0f
+	//);
 }
--- OpenGLPractice/src/Main.cpp
+++ OpenGLPractice/src/Main.cpp
@@ -66,7 +66,7 @@ int main(void)
 	{
 		// Create camera
 		Camera camera;
-		camera.GetViewMatrix();
+		camera.Translate(0.0f, 0.0f, 20.0f);
 
 		// Render content initialization
 		Model model = Model();
@@ -81,7 +81,7 @@ int main(void)
 
 		// Projection matrix
 		glm::mat4 projMatrix = glm::ortho(-50.0f, 50.0f, -50.0f, 50.0f, 0.1f, 100.0f);
-		glm::mat4 viewMatrix = glm::translate(glm::mat4(1.0f), glm::vec3(0.0f, 0.0f, -50.0f));
+		glm::mat4 viewMatrix = camera.GetViewMatrix();
 		glm::mat4 modelMatrix = model.GetModelMatrix();
 		shader.SetUniformMat4f("u_MVP", projMatrix * viewMatrix * modelMatrix);
 
